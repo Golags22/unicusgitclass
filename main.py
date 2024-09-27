508657804de676ba6340f2e804296048b9544580
musicapp <153190928+Golags22@users.noreply.github.com>
2024-09-27 13:22:55 +0100

    This is the initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from utils import randomize_names
+
+
+if __name__ == "__main__":
+    print(__name__)
+    print(randomize_names())
+    
---
+++ utils.py
@@ -0,0 +1,7 @@
+import random 
+
+names =["Emmanuel", "Michaiah", "Rukky"]
+
+def randomize_names():
+    random.shuffle(names)
+    return names
